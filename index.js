7d6ed889902db4d72bfb0bc82e97ce7c5d85d844
Kara Isreal <isreal.kara92@gmail.com>
2018-10-03 15:38:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector){
+return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector('div.target')
+}
+
+function deepestChild() {
+  return document.getElementById('div.grand-node.innerHTML');
+
+ 	}
+
+  function increaseRankBy(n) {
+   const newRank = document.querySelectorAll('.ranked-list');
+   for(var i = 0; i < newRank.length; i++){
+     document.querySelectorAll(".ranked-list")[i].children[i].innerHTML;
+     return parseInt(newRank) + n
+ }
